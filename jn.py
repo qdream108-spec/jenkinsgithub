3240ee17eed985037e9955e6995e2db085604151
unknown <qdream108@gmail.com>
2025-11-24 01:23:40 +0530

    jn.py

---
+++ jn.py
@@ -0,0 +1,7 @@
+print("Hello, Jenkins pipeline from Python! - jn.py:1")
+
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("User - jn.py:5"))
+print("This is a sample Python script for Jenkins integration. - jn.py:6")
+print("End of script. - jn.py:7")
